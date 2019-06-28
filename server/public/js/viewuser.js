61dbf9606b0165b986bc823034dadc04ed82c50c
Barry Allen <barrydevp@gmail.com>
2019-06-28 22:24:52 +0700

    complete list friend in view page and add button unFriend and fix some bug in social page

--- server/public/js/viewuser.js
+++ server/public/js/viewuser.js
@@ -42,6 +42,21 @@ async function unAddRequestFriend() {
 	}
 }
 
+async function unFriend() {
+	const friendId = user.id;
+	const userId = userLogin.id;
+
+	try {
+		let url = '/api/users/delfr/' + friendId;
+		//console.log(url);
+		await axios.patch(url);
+		let divReq = document.querySelector('.profiel-request');
+		divReq.innerHTML = '<button class="btn btn-light mr-3" onclick="addRequestFriend()"><i class="fas fa-user-plus mr-1"></i><span>Add friend</span></button><button class="btn btn-light mr-1"> <i class="fas fa-external-link-square-alt mr-1"></i><span>Follow</span></button><button class="btn btn-light"><i class="fab fa-facebook-messenger mr-1"></i><span>Message</span></button>';
+	} catch(err) {
+		console.error(err);
+	}
+}
+
 async function acceptRequestFriend() {
 	const friendId = user.id;
 	const userId = userLogin.id;
@@ -55,7 +70,7 @@ async function acceptRequestFriend() {
 		reqCount.textContent = parseInt(reqCount.textContent) - 1;
 		liReq.remove();
 		let divReq = document.querySelector('.profiel-request');
-		divReq.innerHTML = '<button class="btn btn-light mr-1"> <i class="fas fa-external-link-square-alt mr-1"></i><span>Follow</span></button><button class="btn btn-light"><i class="fab fa-facebook-messenger mr-1"></i><span>Message</span></button>';
+		divReq.innerHTML = '<button class="btn btn-light mr-3" onclick="unFriend()"><i class="fas fa-user-minus mr-1"></i><span>Unfriend</span></button><button class="btn btn-light mr-1"> <i class="fas fa-external-link-square-alt mr-1"></i><span>Follow</span></button><button class="btn btn-light"><i class="fab fa-facebook-messenger mr-1"></i><span>Message</span></button>';
 	} catch(err) {
 		console.error(err);
 	}
